2697c86a7dbf01a329a5ab9b213b50e4be14e267
Hussain ALFAYLY <2724542a@student.gla.ac.uk>
2023-03-04 01:55:23 +0000

    Fixing up pages and adding forms

--- GameOasis/urls.py
+++ GameOasis/urls.py
@@ -11,12 +11,12 @@ urlpatterns = [
     path('contact-us/', views.contactus, name='contactus'),
     path('shop/', views.shop, name='shop'),
     path('login/', views.user_login, name='login'),
-    path('sign-up/', views.register, name='register'),
+    path('register/', views.register, name='register'),
     path('category/', views.category, name='category'),
     path('checkout/', views.checkout, name='checkout'),
     path('cart/', views.cart, name='cart'),
     path('update_item/', views.updateItem, name='update_item'),
     path('order-success/', views.orderComplete, name='orderComplete'),
-
+    path('logout/', views.user_logout, name='logout'),
 
 ]
--- GameOasis/views.py
+++ GameOasis/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
 from .models import *
-from django.http import JsonResponse
+from .forms import *
+from django.http import JsonResponse, HttpResponse
 import json
 import datetime
-from paypal.standard.models import ST_PP_COMPLETED
-from paypal.standard.ipn.signals import valid_ipn_received
+from django.shortcuts import redirect
 from django.urls import reverse
 from django.shortcuts import render
-from paypal.standard.forms import PayPalPaymentsForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
 def home(request):
 
@@ -25,6 +27,7 @@ def contactus(request):
 
     return render(request, 'GameOasis/contactus.html')
 
+@login_required
 def shop(request):
     if request.user.is_authenticated:
         customer = request.user.customer
@@ -37,26 +40,11 @@ def shop(request):
         order = {'calculate_cart_total': 0, 'calculate_cart_items': 0, 'shipping': False}
         cartItems = order['calculate_cart_items']
 
+
     products = Product.objects.all()
     context = {'products': products, 'cartItems': cartItems}
 
     return render(request, 'GameOasis/shop.html', context)
-
-
-
-def user_login(request):
-
-
-
-    return render(request, 'GameOasis/login.html')
-
-
-
-def register(request):
-
-
-
-    return render(request, 'GameOasis/register.html')
 
 
 def category(request):
@@ -151,5 +139,70 @@ def orderComplete(request):
 
     return JsonResponse('Payment complete!', safe=False)
 
+def user_login(request):
+    if request.method == 'POST':
+        #Takes the username and password inputted to authenticate
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
+        #Using django engine it checks if this combination is valid and returns the User object if it is
+        user = authenticate(username=username, password=password)
+
+        #Now if the user object is in our database it will check if it is active or not
+        if user:
+            if user.is_active:
+                #user may login if active
+                login(request,user)
+                return redirect(reverse('GameOasis:home'))
+
+            else:
+                #else account inactive so no logging in
+                return HttpResponse("Your account is disabled")
+
+        else:
+            #invalid login details provided so no logging in
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+
+    #if the scenario was a HTTP GET
+    else:
+        #No context variables to return hence blank dictionary object so no third parameter
+        return render(request, 'GameOasis/login.html')
+
+
+def register(request):
+    #Boolean value to indicate if the registration was successful or not
+    #Set to false initially then changes to true when it is successful
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        if user_form.is_valid():
+            #Saves the users form data to the database if valid
+            user = user_form.save()
+            #Now we hash the user password and then update the user object
+            user.set_password(user.password)
+            user.save()
+            customer = Customer.objects.create(user=user, name=user.username, email=user.email)
+
+            #Updated registered to show that the form has been successful
+            registered = True
+
+        else:
+            #prints any mistakes/invalid forms in the terminal
+            print(user_form.errors)
+
+    else:
+        #Not a HTTP Post so we render the form using the two ModelForm instances so they are ready for user input
+        user_form = UserForm()
+
+    return render(request, 'GameOasis/register.html', context = {'user_form': user_form, 'registered': registered})
+
+
+@login_required
+def user_logout(request):
+    #Since we know the user is logged in we can now just log them out
+    logout(request)
+    #Takes the user back to the homepage
+    return redirect(reverse('GameOasis:home'))
 
